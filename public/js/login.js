8d9fb0c721eea5a9487edc08540166f27ce52ec9
JaredTempleton <95387966+JaredTempleton@users.noreply.github.com>
2022-06-02 01:08:17 -0400

    added logic

--- public/js/login.js
+++ public/js/login.js
@@ -1,11 +1,11 @@
-async function signupFormHandler(event) {
+async function loginFormHandler(event) {
     event.preventDefault();
 
-    const username = document.querySelector('#username-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+    const username = document.querySelector('#username-login').value;
+    const password = document.querySelector('#password-login').value;
 
     if (username && password) {
-        const response = await fetch('/api/users', {
+        const response = await fetch('/api/users/login', {
             method: 'POST',
             body: JSON.stringify({
                 username, password
@@ -16,7 +16,6 @@ async function signupFormHandler(event) {
         });
 
         if (response.ok){
-            console.log('Login Successful')
             document.location.replace('/dashboard');
         } else {
             alert(response.statusText);
@@ -24,4 +23,4 @@ async function signupFormHandler(event) {
     };
 };
 
-document.querySelector('#signup-form').addEventListener('submit', signupFormHandler);
+document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
